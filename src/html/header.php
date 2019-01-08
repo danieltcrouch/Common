fe8ea175311dffeceb304d657c21896fc83d2aed
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-08 16:15:05 -0600

    Clean-up, pt. 1

---
+++ src/html/header.php
@@ -0,0 +1,5 @@
+<!--Menu-->
+<div class="col-10 navBar">
+    <a href="<?php echo $homeUrl ?>"><div class="col-5 navButton center">Home</div></a>
+    <a href="http://religionandstory.com/"><div class="col-5 navButton center">Religion &amp; Story</div></a>
+</div>
--- src/php/startup.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$BASE_UTL_PATH = $_SERVER["DOCUMENT_ROOT"] . "/utility";
-?>
