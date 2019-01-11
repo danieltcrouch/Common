e69c29b0533a3deef6c5762585cbdf8acea05771
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-01-11 08:39:06 -0600

    Fixed http

--- src/html/header.php
+++ src/html/header.php
@@ -2,5 +2,5 @@
 <!--Menu-->
 <div class="col-10 navBar">
     <a href="<?php echo $homeUrl ?>"><div class="col-5 navButton center">Home</div></a>
-    <a href="http://religionandstory.com/"><div class="col-5 navButton center">Religion &amp; Story</div></a>
+    <a href="https://religionandstory.com/"><div class="col-5 navButton center">Religion &amp; Story</div></a>
 </div>
