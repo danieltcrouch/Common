3c88a27e106a524d8db58843742a7d025ada4589
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-06-21 15:16:12 -0500

    Added static radio buttons

---
+++ src/javascript/utility.js
@@ -0,0 +1,11 @@
+function id( idName ) {
+    return document.getElementById( idName );
+}
+
+function cl( className ) {
+    return document.getElementsByClassName( className );
+}
+
+function nm( nameName ) {
+    return document.getElementsByName( nameName );
+}
